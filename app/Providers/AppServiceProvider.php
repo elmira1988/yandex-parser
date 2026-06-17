3cd4f2a4ef6ec37a3741fc813b8e4cc7684d20d8
elmira <elmira.sharapova@yandex.ru>
2026-06-17 21:28:50 +0500

    fix: принудительный HTTPS для пагинации Inertia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
     {
         Vite::prefetch(concurrency: 3);
         if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
+
+        // Если проект работает в окружении production, принудительно переводим все ссылки на HTTPS
+        if (config('app.env') === 'production' || env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
